21a50a11093c0be0b292e61636a2ab9b654dbb62
Thales T <thales.toniate@yahoo.com.br>
2025-06-09 17:17:07 -0300

    Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
-
 export function App() {
   return (
-    <div>
-      <Heading>
-        Ola mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui modi atque
-        nihil cupiditate perspiciatis non accusantium, libero quas velit
-        voluptatem veniam, incidunt natus reiciendis nostrum nesciunt. Eaque
-        quaerat voluptatum praesentium.
-      </p>
-    </div>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+    </>
   );
 }
 
