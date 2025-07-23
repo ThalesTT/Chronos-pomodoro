a0132b57d3b4bf838b44b7d1a885470e96779b94
Thales T <thales.toniate@yahoo.com.br>
2025-07-23 17:20:35 -0300

    Messages

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../Tips';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -22,12 +23,13 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLElement>) {
     event.preventDefault();
+    showMessage.dismiss();
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Digite a tarefa!');
+      showMessage.warn('Digite uma tarefa');
       return;
     }
 
@@ -42,12 +44,15 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+    showMessage.success('Tarefa iniciada');
   }
 
   function handleInterruptTask(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) {
     e.preventDefault();
+    showMessage.dismiss();
+    showMessage.info('Tarefa interrompida');
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
   return (
